3db759c41da85a043c176e9a0f0de7a11d89dc6a
diegovalverde <diego.valverde.g@gmail.com>
2015-08-28 15:37:58 -0600

    add: dev: Initial folder structure

---
+++ bin/codewars.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+"""
+codewars
+"""
+
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+
+import sys
+if "C:\\Users\\valverdi\\Google Drive\\projects\\codewars\\codewars" not in sys.path:
+    sys.path.append("C:\\Users\\valverdi\\Google Drive\\projects\\codewars\\codewars")
+
+
+
+if __name__ == '__main__':
+
+ #   from codewars.args import parse_args
+    from codewars.main import main
+
+ #   args = parse_args()
+    print('Starting code wars')
+  #  main(args)
+   
---
+++ codewars/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+
+"""
+codewars version.
+"""
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import absolute_import
+
+__version__ = '0.0.1'
+
+__all__ = ['__version__']
